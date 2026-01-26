e45b744384f304239b6db6bfbf4488f20395160c
phuchgce181933 <phuchgce181933@gmail.com>
2026-01-26 09:26:23 +0700

    cấu_trúc_chung

---
+++ src/main/java/com/ra/batshop/BatshopApplication.java
@@ -0,0 +1,13 @@
+package com.ra.batshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatshopApplication.class, args);
+    }
+
+}
