7144e002bbc136d0c2bf222f7f058256665707ce
MinhQ <minhq@gmail.com>
2026-01-27 21:09:47 +0700

    manage_user

--- src/main/java/com/ra/batshop/controller/AdminController.java
+++ src/main/java/com/ra/batshop/controller/AdminController.java
@@ -1,15 +1,29 @@
 package com.ra.batshop.controller;
 
+import com.ra.batshop.model.User;
+import com.ra.batshop.repository.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
+    private final UserRepository userRepository;
+    public AdminController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/admin")
     public String dashboard() {
         return "admin/dmin";
     }
+
+    @GetMapping("/admin/manageruser")
+    public String userPage(Model model) {
+        model.addAttribute("user", userRepository.findAll());
+        return "admin/user";
+    }
+
 
 }
