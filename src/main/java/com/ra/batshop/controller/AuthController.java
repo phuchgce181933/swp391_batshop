dc288082396b562dfe52b6b54039dbfdcc9026a2
phuchgce181933 <phuchgce181933@gmail.com>
2026-01-27 20:04:07 +0700

    home

---
+++ src/main/java/com/ra/batshop/controller/AuthController.java
@@ -0,0 +1,78 @@
+package com.ra.batshop.controller;
+
+
+import com.ra.batshop.model.Enum.Role;
+import com.ra.batshop.model.User;
+import com.ra.batshop.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class AuthController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UserRepository userRepository,
+                          PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    // Hiển thị form
+    @GetMapping("/register")
+    public String showRegister(Model model) {
+        model.addAttribute("user", new User());
+        return "auth/register";
+    }
+
+    // Xử lý đăng ký
+    @PostMapping("/register")
+    public String register(
+            @ModelAttribute("user") User user,
+            Model model
+    ) {
+        // 1. Check email tồn tại
+        if (userRepository.existsByEmail(user.getEmail())) {
+            model.addAttribute("error", "Email đã tồn tại");
+            return "auth/register";
+        }
+        // 2. Hash password
+        user.setPasswordHash(passwordEncoder.encode(user.getPasswordHash())
+        );
+        // 3. Set mặc định
+        user.setRole(Role.USER);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+
+        userRepository.save(user);
+
+        return "redirect:/login";
+    }
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        return "auth/login";
+    }
+    @PostMapping("/login")
+    public String doLogin(@RequestParam String email,
+                          @RequestParam String password, Model model, HttpSession session) {
+        User user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) {
+            model.addAttribute("error", "User not found");
+            return "auth/login";
+        }
+        if (!passwordEncoder.matches(password, user.getPasswordHash())) {
+            model.addAttribute("error", "Wrong password");
+            return "auth/login";
+        }
+        session.setAttribute("user", user);
+        return "redirect:/home";
+    }
+}
---
+++ src/main/java/com/ra/batshop/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.ra.batshop.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(HttpSession session) {
+
+//        // (khuyến nghị) check login
+//        if (session.getAttribute("user") == null) {
+//            return "redirect:/login";
+//        }
+
+        return "home";
+    }
+}
