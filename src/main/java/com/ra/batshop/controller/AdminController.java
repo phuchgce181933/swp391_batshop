3883aa7fc66c72dc689af2c5f65ce4ac2a77cf5a
PhuocNNT <phuocnntce182513@fpt.edu.vn>
2026-01-29 14:06:48 +0700

    add 33 empty html, and modified 9 others html

--- src/main/java/com/ra/batshop/controller/AdminController.java
+++ src/main/java/com/ra/batshop/controller/AdminController.java
@@ -1,11 +1,9 @@
 package com.ra.batshop.controller;
 
-import com.ra.batshop.model.User;
 import com.ra.batshop.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
@@ -22,7 +20,7 @@ public class AdminController {
     @GetMapping("/admin/manageruser")
     public String userPage(Model model) {
         model.addAttribute("user", userRepository.findAll());
-        return "admin/user";
+        return "Account-Management";
     }
 
 
