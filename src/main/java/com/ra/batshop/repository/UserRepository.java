0bc37ab5a1b36e5ae12002ba6a3c9386ac50de6c
huydn2ce190894-cpu <huydn2.ce190894@gmail.com>
2026-01-31 15:36:01 +0700

    thong bao sai

--- src/main/java/com/ra/batshop/repository/UserRepository.java
+++ src/main/java/com/ra/batshop/repository/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
+
+
 }
