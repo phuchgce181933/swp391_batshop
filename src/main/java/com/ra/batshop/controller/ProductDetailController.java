d98541a5c273bc57b2c3ec357deebdfe91bf5ab5
loidhce180748 <loidhce180748@fpt.edu.vn>
2026-01-31 19:06:34 +0700

    update_productdetail

--- src/main/java/com/ra/batshop/controller/ProductDetailController.java
+++ src/main/java/com/ra/batshop/controller/ProductDetailController.java
@@ -1,31 +1,44 @@
 package com.ra.batshop.controller;
 
 import com.ra.batshop.model.Product;
+import com.ra.batshop.model.ProductVariant;
 import com.ra.batshop.repository.ProductRepository;
+import com.ra.batshop.repository.ProductVariantRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/product")
 public class ProductDetailController {
 
     private final ProductRepository productRepository;
+    private final ProductVariantRepository variantRepository;
 
-    public ProductDetailController(ProductRepository productRepository) {
+    public ProductDetailController(ProductRepository productRepository,
+                                   ProductVariantRepository variantRepository) {
         this.productRepository = productRepository;
+        this.variantRepository = variantRepository;
     }
-
-    // View product detail
+    @GetMapping("/productvariant/list")
+    public String ProductVariantList(Model model) {
+        model.addAttribute("productvariant", variantRepository.findAll());
+        return ("user/productvariant-list");
+    }
+    // VIEW PRODUCT DETAIL
     @GetMapping("/detail/{id}")
-    public String viewProductDetail(@PathVariable Integer id, Model model) {
+    public String productDetail(@PathVariable Integer id, Model model) {
 
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Product not found"));
 
+        List<ProductVariant> variants = variantRepository.findByProduct_Id(id);
+
         model.addAttribute("product", product);
+        model.addAttribute("variants", variants);
+
         return "user/product-detail";
     }
 }
