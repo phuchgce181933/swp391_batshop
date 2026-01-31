7828a2b1b7806cc3a2ed22746fd8502615c4fbf3
sikyee <Norasikyee@gmail.com>
2026-01-31 15:20:05 +0700

    add product list for customer

--- src/main/java/com/ra/batshop/controller/HomeController.java
+++ src/main/java/com/ra/batshop/controller/HomeController.java
@@ -1,16 +1,32 @@
 package com.ra.batshop.controller;
 
+import com.ra.batshop.repository.ProductRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/home")
 @Controller
 public class HomeController {
-    @GetMapping("/home")
+    private final ProductRepository productRepository;
+    public HomeController(ProductRepository productRepository) {
+
+        this.productRepository = productRepository;
+    }
+    @GetMapping()
     public String home(HttpSession session) {
         if (session.getAttribute("user") == null) {
             return "redirect:/login";
         }
         return "home";
     }
-}
+
+    @GetMapping("/product")
+    public String product(Model model) {
+            model.addAttribute("products", productRepository.findAll());
+            return "user/product";
+        }
+    }
+
