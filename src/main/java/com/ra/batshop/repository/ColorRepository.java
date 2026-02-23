7ca81a8eede59523a688665dc29dd5a9094d7866
phuchgce181933 <phuchgce181933@gmail.com>
2026-02-23 17:34:34 +0700

    managementProduct

---
+++ src/main/java/com/ra/batshop/repository/ColorRepository.java
@@ -0,0 +1,7 @@
+package com.ra.batshop.repository;
+
+import com.ra.batshop.model.Color;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ColorRepository extends CrudRepository<Color, Integer> {
+}
---
+++ src/main/java/com/ra/batshop/repository/SizeRepository.java
@@ -0,0 +1,7 @@
+package com.ra.batshop.repository;
+
+import com.ra.batshop.model.Size;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SizeRepository extends JpaRepository<Size, Integer> {
+}
