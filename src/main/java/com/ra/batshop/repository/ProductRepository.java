6e8517046c3dda632b34ca261e4f660d78ec98a2
loidhce180748 <loidhce180748@fpt.edu.vn>
2026-03-02 03:33:13 +0700

    add_filter_and_view_detail_product

--- src/main/java/com/ra/batshop/repository/ProductRepository.java
+++ src/main/java/com/ra/batshop/repository/ProductRepository.java
@@ -3,5 +3,16 @@ package com.ra.batshop.repository;
 import com.ra.batshop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findByCategory_Id(Integer categoryId);
+    List<Product> findByStatusTrue();
+
+    List<Product> findByCategory_IdAndStatusTrue(Integer categoryId);
+
+    List<Product> findByCategory_IdAndBrand_IdAndStatusTrue(
+            Integer categoryId, Integer brandId);
+
+    List<Product> findByBrand_IdAndStatusTrue(Integer brandId);
 }
